ca61ed97b0639f26f8a3746b170ea0917225ee9d
trapjaw81 <trapjaw81@gmail.com>
2023-04-24 16:23:25 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-bm8f9e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-bm8f9e)
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkDragDropConnectedSortingExample} from './cdk-drag-drop-connected-sorting-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkDragDropConnectedSortingExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  bootstrap: [CdkDragDropConnectedSortingExample],
+})
+export class AppModule {}
